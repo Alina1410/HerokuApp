7f864bb4e4ed5fc3c296e23325dbe216692c4e36
Alina <a.kozhunova@yandex.ru>
2023-06-27 13:26:34 +0300

    pages and tests

--- src/test/java/lesson/CheckboxesTest.java
+++ src/test/java/lesson/CheckboxesTest.java
@@ -1,3 +1,6 @@
+package lesson;
+
+import lesson.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
--- src/test/java/lesson/DropdownTest.java
+++ src/test/java/lesson/DropdownTest.java
@@ -1,3 +1,6 @@
+package lesson;
+
+import lesson.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
